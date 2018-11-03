e4ac7f3a6e9918500863e2b29c47a254b92b3659
Gajendra D Ambi <gajuambi@gmail.com>
2018-11-04 02:56:16 +0530

    Update centos7startup.sh

--- centos7startup.sh
+++ centos7startup.sh
@@ -2,13 +2,12 @@ cd /home/
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y perl gcc dkms kernel-devel kernel-headers make zip unzip bzip2 wget git kernel-devel kernel* kernel-devel-$(uname -r) open-vm-tools
+yum install -y perl gcc dkms kernel-devel kernel-headers make zip unzip bzip2 wget git kernel-devel kernel* kernel-devel-$(uname -r) open-vm-tools python36u python36u-libs python36u-devel python36u-pip
 yum update -y
 ls -l /usr/src/kernels/$(uname -r)
 wget -O atom.rpm https://atom.io/download/rpm/
 chmod +x atom.rpm 
-yum install -y atom.rpm 
-yum install -y python36u python36u-libs python36u-devel python36u-pip
+yum install -y atom.rpm
 alias python=python3.6
 alias pip=pip3.6
 source ~/.bashrc
